db413d2743dbcf15032765165bb3f0c0b66e5dff
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-05-27 18:17:02 +0200

    finished quest 05

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,18 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+create table category (
+  id int unsigned primary key auto_increment not null,
+  name varchar(80) not null
+);
+create table program (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  synopsis TEXT NOT NULL,
+  poster VARCHAR(255) NOT NULL,
+  country VARCHAR(80) NOT NULL,
+  year INT NOT NULL,
+  category_id int unsigned not null,
+  foreign key (category_id) references user(id)
+);
